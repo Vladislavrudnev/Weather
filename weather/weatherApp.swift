d55092d2c8fdba1426010828a2cad0e0a715a376
Владислав <92315082+Vladislavrudnev@users.noreply.github.com>
2022-10-07 13:57:56 +0600

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by владислав Руднев on 07.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
